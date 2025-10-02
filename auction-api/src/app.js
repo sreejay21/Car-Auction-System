5c686708bca5b64e53cd22e3d22d86249c653892
Sreejay Sreekumar <ssreejay796@gmail.com>
2025-10-02 12:46:14 +0530

    Folder Structure

---
+++ auction-api/src/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("../src/config/db");
+
+dotenv.config();
+connectDB();
+
+// const authRoutes = require("./routes/authRoutes");
+// const auctionRoutes = require("./routes/auctionRoutes");
+
+const app = express();
+app.use(express.json());
+
+// app.use("/api/v1/auction", auctionRoutes);
+// app.use("/api/v1/auction", authRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
+
+module.exports = app;
